4452b2fe4280f857bf65da400b55e77478a8fb5c
Timothy Pfafman <tim@pfafman.com>
2021-08-20 09:35:48 -0600

    new meteor apis

--- package.js
+++ package.js
@@ -1,35 +1,35 @@
 Package.describe({
   name: 'iron:location',
   summary: 'Reactive urls that work in IE8/9 and modern browsers.',
-  version: '1.0.11',
+  version: '1.0.11_4',
   git: 'https://github.com/eventedmind/iron-location.git'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.2');
+Package.onUse(function (api) {
+  //api.versionsFrom('METEOR@0.9.2');
 
   api.use('underscore');
   api.use('tracker');
   api.use('jquery');
 
-  api.use('iron:core@1.0.11');
+  api.use('iron:core'); // @1.0.11');
   api.imply('iron:core');
 
-  api.use('iron:url@1.0.11');
+  api.use('iron:url'); //@1.0.11');
 
   api.use('appcache', {weak: true});
 
-  api.add_files('lib/utils.js', 'client');
-  api.add_files('lib/state.js', 'client');
-  api.add_files('lib/location.js', 'client');
+  api.addFiles('lib/utils.js', 'client');
+  api.addFiles('lib/state.js', 'client');
+  api.addFiles('lib/location.js', 'client');
 
   api.export(['urlToHashStyle', 'urlFromHashStyle'], 'client', {testOnly: true});
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('iron:location');
   api.use('tinytest');
   api.use('test-helpers');
 
-  api.add_files('test/location_test.js', 'client');
+  api.addFiles('test/location_test.js', 'client');
 });
